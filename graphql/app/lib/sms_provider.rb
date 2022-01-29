f85e5bf61266ce91aeb4662afe3584f39cca4ce4
Dorokhovich Michael <mikhail@dorokhovich.ru>
2022-01-29 17:40:04 +0300

    added sms provider

--- graphql/app/lib/sms_provider.rb
+++ graphql/app/lib/sms_provider.rb
@@ -1,3 +1,11 @@
 class SmsProvider
+  @client = Twilio::REST::Client.new
 
+  def send_sms(to:, message:)
+    @client.messages.create(
+      from: Rails.application.credentials.twilio_phone_number,
+      to: to,
+      body: message
+    )
+  end
 end
