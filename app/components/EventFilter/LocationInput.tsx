2c2a82dcc63d852652c65b7f58d318f35786955d
maxhooga <mahooga@gmail.com>
2022-07-05 10:41:18 +0200

    input => done

--- app/components/EventFilter/LocationInput.tsx
+++ app/components/EventFilter/LocationInput.tsx
@@ -3,23 +3,37 @@ import styled from "styled-components";
 import icon from "../../components/icons/Outline/Interface/Edit.svg";
 import Image from "next/image";
 
-const Wrapper = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
+const Img = styled.div`
+  display: none;
 `;
 
 const Input = styled.input`
-    border: none;
-    font-family: 'Inter';
-    font-size: 36px;
-    font-weight: 400;
-    line-height: 44px;
-    width: 100%;
-    &:focus {
-        outline: none;
-        border-bottom: 1px solid black;
-    }
+  border: none;
+  border-bottom: 1px solid transparent;
+  font-family: 'Inter';
+  font-size: 36px;
+  font-weight: 400;
+  line-height: 44px;
+  width: 100%;
+  &:focus {
+      outline: none;
+      border-bottom: 1px solid black;
+  }
+`;
+
+const Wrapper = styled.div`
+  
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-end;
+  &:hover ${Img} {
+    display: block;
+    border-bottom: 1px solid black;
+  }
+  &:hover ${Input} {
+    border-bottom: 1px solid black;
+  }
 `;
 
 function LocationInput() {
@@ -30,12 +44,14 @@ function LocationInput() {
             type="text"
             placeholder="search location"
         />
-        <Image
-          src={icon.src}
-          alt="Picture of the author"
-          width={22}
-          height={22}
-        />        
+        <Img>
+            <Image
+              src={icon.src}
+              alt="Picture of the author"
+              width={46}
+              height={46}
+            />
+        </Img>
     </Wrapper>
   );
 }
