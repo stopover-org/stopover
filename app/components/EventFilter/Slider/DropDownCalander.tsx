69dc55671bec50f74695aaac55c8ac2f4364210d
maxhooga <mahooga@gmail.com>
2022-07-09 17:58:48 +0200

    calender looks like in design. Need to add svg

--- app/components/EventFilter/Slider/DropDownCalander.tsx
+++ app/components/EventFilter/Slider/DropDownCalander.tsx
@@ -5,8 +5,39 @@ import { DateRangePicker, FocusedInputShape } from 'react-dates';
 import moment from 'moment';
 import 'react-dates/lib/css/_datepicker.css';
 
+const Wrapper = styled.div`
+  .DateInput{
+    width: 170px;
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 29px;
+  }
+  
+  .DateInput_input{    
+    border: 2px solid #FF8A00;
+    height: 50px;
+    margin: -0px;
+  }
+
+  .DateRangePickerInput__withBorder{
+    border: none;
+    width: 390px;
+    display: flex;
+    justify-content: space-between;
+  }
+  .DateRangePickerInput_arrow_svg{
+    display: none;
+  }
+  .DateRangePickerInput_arrow{
+    display: none;
+  }
+
+`;
+
 function DropDownCalander() {
-    const [startDate, setStartDate] = useState<moment.Moment | null>(moment());
+    const [startDate, setStartDate] = useState<moment.Moment | null>(null);
     const [endDate, setEndDate] = useState<moment.Moment | null>(null);
     const [focusedInput, setFocusedInput] = useState<FocusedInputShape | null>(null);
 
@@ -23,15 +54,21 @@ function DropDownCalander() {
     }
 
     return (
-      <DateRangePicker
-        startDate={startDate} // moment.Moment | null;
-        startDateId="your_unique_start_date_id" // moment.Moment | null;
-        endDate={endDate} // momentPropTypes.momentObj or null,
-        endDateId="your_unique_end_date_id" // string;
-        onDatesChange={handlendDatesChange} // (arg: { startDate: moment.Moment | null; endDate: moment.Moment | null }) => void;
-        focusedInput={focusedInput} // FocusedInputShape | null;
-        onFocusChange={handleFocusChange} // (arg: FocusedInputShape | null) => void;
+      <Wrapper>
+        <DateRangePicker
+          startDate={startDate} // moment.Moment | null;
+          startDateId="your_unique_start_date_id" // moment.Moment | null;
+          endDate={endDate} // momentPropTypes.momentObj or null,
+          endDateId="your_unique_end_date_id" // string;
+          onDatesChange={handlendDatesChange} // (arg: { startDate: moment.Moment | null; endDate: moment.Moment | null }) => void;
+          focusedInput={focusedInput} // FocusedInputShape | null;
+          onFocusChange={handleFocusChange} // (arg: FocusedInputShape | null) => void;
+
+          startDatePlaceholderText={"Начало"}
+          endDatePlaceholderText={"Конец"}
       />
+      </Wrapper>
+      
     )
 }
 
