b5c794853d56347a04b8e9f3e0f573b9727fb02f
Dorokhovich Michael <mikhail@dorokhovich.ru>
2022-09-02 21:23:44 +0400

    fix something

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import Layout from "../components/MainPage/Layout";
 import EventsList from "../components/Events/List";
 
-// eslint-disable-next-line react/function-component-definition
 const Home: NextPage = () => (
   <Layout>
     <EventsList />
