69368f34360c959a91774de3fa0a7262fcbcc674
Dorokhovich Michael <mikhail@dorokhovich.ru>
2022-09-07 20:30:36 +0400

    added edit form

--- app/components/Admin/Events/Edit.tsx
+++ app/components/Admin/Events/Edit.tsx
@@ -1,13 +1,29 @@
-import { Edit, SimpleForm } from 'react-admin'
+import { Edit, SimpleForm, TextInput, NumberInput } from "react-admin";
 
-export const EditEvent = () => {
-  return (
-    <Edit>
-      <SimpleForm>
+export const EditEvent = () => (
+  <Edit>
+    <SimpleForm>
+      <TextInput name="title" source="title" />
+      <TextInput name="description" source="description" />
+      <NumberInput
+        name="organizer_cost_per_uom_cents"
+        source="organizer_cost_per_uom_cents"
+      />
+      <NumberInput
+        name="attendee_cost_per_uom_cents"
+        source="attendee_cost_per_uom_cents"
+      />
+      <TextInput name="duration_time" source="duration_time" />
+      <TextInput name="house_number" source="house_number" />
+      <TextInput name="street" source="street" />
+      <TextInput name="city" source="city" />
+      <TextInput name="country" source="country" />
+      <TextInput name="region" source="region" />
+      <TextInput name="full_address" source="full_address" />
+      <NumberInput name="longitude" source="longitude" />
+      <NumberInput name="latitude" source="latitude" />
+    </SimpleForm>
+  </Edit>
+);
 
-      </SimpleForm>
-    </Edit>
-  )
-}
-
-export default EditEvent
+export default EditEvent;
