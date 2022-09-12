bab94fbb68026905a02cc0e76a926cc5fd6a6be2
maxhooga <mahooga@gmail.com>
2022-09-12 09:00:27 +0200

    component typography done

---
+++ app/components/StatesEnum.tsx
@@ -0,0 +1,19 @@
+export enum TypographyVariants {
+  HEADER,
+  COMMON_TEXT,
+}
+
+export enum TypographySize {
+  H1 = "36px",
+  H2 = "32px",
+  H3 = "24px",
+  H4 = "18px",
+  H5 = "14px",
+  H6 = "14px",
+  VERY_SMALL = "8px",
+  SMALL = "10px",
+  MEDIUM = "14px",
+  BIG = "16px",
+  LARGE = "24px",
+  VERY_LARGE = "36px",
+}
