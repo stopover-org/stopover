26078b7a645937e841698988bdba5449cc40a77b
maxhooga <mahooga@gmail.com>
2022-09-15 13:16:57 +0200

    component Row, Column added

--- app/components/Row/index.tsx
+++ app/components/Row/index.tsx
@@ -1,19 +1,60 @@
+import React from "react";
 import styled from "styled-components";
 
-const Wrapper = styled.div<Props>`
+const Wrapper = styled.div<{
+  justifyContent: string;
+  alignItems: string;
+  paddingLeft: string;
+  paddingTop: string;
+  paddingRight: string;
+  paddingBottom: string;
+  padding: string;
+}>`
   display: flex;
   flex-direction: row;
-  width: ${(props) => props.width || "100%"};
-  height: ${(props) => props.height || "100%"};
-  justify-content: ${(props) => props.justifyContent || "center"};
-  align-items: ${(props) => props.alignItems || "center"};
+  width: 100%;
+  height: 100%;
+  justify-content: ${(props) => props.justifyContent};
+  align-items: ${(props) => props.alignItems};
+  padding-left: ${(props) => props.paddingLeft};
+  padding-top: ${(props) => props.paddingTop};
+  padding-right: ${(props) => props.paddingRight};
+  padding-bottom: ${(props) => props.paddingBottom};
+  padding: ${(props) => props.padding};
 `;
 
 type Props = {
+  children: React.ReactElement;
   justifyContent?: string;
   alignItems?: string;
-  width?: string;
-  height?: string;
+  paddingLeft?: string;
+  paddingTop?: string;
+  paddingRight?: string;
+  paddingBottom?: string;
+  padding?: string;
 };
 
-export default Wrapper;
+const Row = ({
+  children,
+  justifyContent = "center",
+  alignItems = "center",
+  paddingLeft = "0px",
+  paddingTop = "0px",
+  paddingRight = "0px",
+  paddingBottom = "0px",
+  padding = "0px",
+}: Props) => (
+  <Wrapper
+    justifyContent={justifyContent}
+    alignItems={alignItems}
+    paddingLeft={paddingLeft}
+    paddingTop={paddingTop}
+    paddingRight={paddingRight}
+    paddingBottom={paddingBottom}
+    padding={padding}
+  >
+    {children}
+  </Wrapper>
+);
+
+export default Row;
---
+++ app/components/Typography/index.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import styled from "styled-components";
+import { TypographySize, TypographyTags } from "./StatesEnum";
+
+const Wrapper = styled.div``;
+
+const TextStyle = styled.span<{
+  size: string;
+  textDecoration: string;
+  fontWeight: string;
+  fontStyle: string;
+  color: string;
+}>`
+  display: flex;
+  justify-content: center;
+  font-family: "Roboto";
+  font-style: ${(props) => props.fontStyle};
+  font-weight: ${(props) => props.fontWeight};
+  font-size: ${(props) => props.size};
+  text-decoration: ${(props) => props.textDecoration};
+  color: ${(props) => props.color};
+  line-height: 1em;
+`;
+
+type Props = {
+  children: string | React.ReactElement;
+  size?: TypographySize;
+  as: TypographyTags;
+  strikeThrough?: boolean;
+  bold?: boolean;
+  italic?: boolean;
+  color?: string;
+};
+
+const Typography = ({
+  color,
+  italic,
+  bold,
+  strikeThrough,
+  size,
+  children,
+  as = TypographyTags.MEDIUM,
+}: Props) => (
+  <Wrapper className="typography-wrapper">
+    <TextStyle
+      as={as}
+      size={size || TypographySize.MEDIUM.toString()}
+      textDecoration={strikeThrough ? "line-through" : "none"}
+      fontWeight={bold ? "700" : "none"}
+      fontStyle={italic ? "italic" : "none"}
+      color={color || "black"}
+    >
+      {children}
+    </TextStyle>
+  </Wrapper>
+);
+
+export default Typography;
