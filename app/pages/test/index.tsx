5804e0824a1eebc90b20603e5a376c42f04782c2
maxhooga <51508706+maxhooga@users.noreply.github.com>
2022-09-23 17:44:19 +0200

    Accordion done (#33)
    * acardion done
    
    * small fixes
    
    * removed console
    
    Co-authored-by: maxhooga <mahooga@gmail.com>

--- app/pages/test/index.tsx
+++ app/pages/test/index.tsx
@@ -1,26 +1,78 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import styled from "styled-components";
 import Layout from "../../components/MainPage/Layout";
-import Acardion from "../../components/Acardion";
+import Accordion from "../../components/Accordion";
 import Typography from "../../components/Typography";
 import { TypographySize, TypographyTags } from "../../components/StatesEnum";
 import Column from "../../components/Column";
+import Row from "../../components/Row";
 
-const Test = () => (
-  <Layout>
-    <Acardion
-      opened
-      header={
-        <Typography size={TypographySize.H1} as={TypographyTags.H1}>
-          hi
-        </Typography>
-      }
-      content={
-        <Typography size={TypographySize.H1} as={TypographyTags.H1}>
-          <Column>some content</Column>
-        </Typography>
-      }
-    />
-  </Layout>
-);
+const AccordionStyle = styled.div`
+  width: 500px;
+`;
 
+const Test = () => {
+  const [contentHeightState, setContentHeightState] = useState(0);
+  const onOpen = () => {};
+  const onClose = () => {};
+  const contentHeight = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (contentHeight.current) {
+      setContentHeightState(contentHeight.current!.offsetHeight);
+    }
+  }, []);
+
+  return (
+    <Layout>
+      <AccordionStyle>
+        <Accordion
+          contentHeight={contentHeightState}
+          opened
+          header={
+            <Typography size={TypographySize.H1} as={TypographyTags.H1}>
+              hi
+            </Typography>
+          }
+          content={
+            <Column
+              ref={contentHeight}
+              justifyContent="start"
+              alignItems="start"
+            >
+              <Row justifyContent="start">
+                <Typography size={TypographySize.H6} as={TypographyTags.H1}>
+                  some
+                  coadsdddnssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssstent
+                </Typography>
+              </Row>
+              <Row justifyContent="start">
+                <Typography size={TypographySize.H6} as={TypographyTags.H1}>
+                  somdsfae content
+                </Typography>
+              </Row>
+              <Row justifyContent="start">
+                <Typography size={TypographySize.H6} as={TypographyTags.H1}>
+                  somdsafdsfasde content
+                </Typography>
+              </Row>
+              <Row justifyContent="start">
+                <Typography size={TypographySize.H6} as={TypographyTags.H1}>
+                  somedsafasdfsda content
+                </Typography>
+              </Row>
+              <Row justifyContent="start">
+                <Typography size={TypographySize.H6} as={TypographyTags.H1}>
+                  somefdsafsd content
+                </Typography>
+              </Row>
+            </Column>
+          }
+          onOpen={onOpen}
+          onClose={onClose}
+        />
+      </AccordionStyle>
+    </Layout>
+  );
+};
 export default Test;
