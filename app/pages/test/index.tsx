8a16313c927c3c0664c59c7833a0b55bb088335b
maxhooga <mahooga@gmail.com>
2022-09-28 16:59:14 +0200

    input date properties added

--- app/pages/test/index.tsx
+++ app/pages/test/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import moment from "moment";
 import Layout from "../../components/MainPage/Layout";
