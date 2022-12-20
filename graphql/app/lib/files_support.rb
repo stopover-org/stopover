30f36614123258569e499c6b2f5e1072bdab9401
maxhooga <51508706+maxhooga@users.noreply.github.com>
2022-12-20 15:12:05 +0100

    Draft: sftp integration (#64)
    * wip: started sftp integration
    
    * wip: fixed conflicts
    
    * wip: retrieving files done

--- graphql/app/lib/files_support.rb
+++ graphql/app/lib/files_support.rb
@@ -20,4 +20,31 @@ class FilesSupport
       original_filename: "#{filename}.jpg"
     )
   end
+
+  def self.get_files_recursively(sftp:, folders:, files:)
+    sftp.dir.foreach(folders) do |entry|
+      next if entry.name == '.' || entry.name == '..'
+      if File.directory?("#{folders[0]}/#{entry.name}")
+        folders << "#{folders[0]}/#{entry.name}"
+      else
+        files << "#{folders[0]}/#{entry.name}"
+      end
+    end
+
+    folders.shift
+    return files if folders.empty?
+
+    get_files_recursively(sftp: sftp, folders: folders, files: files)
+  end
+
+  def self.retrieve_files(host:, username:, password:, remote_path:)
+    files = []
+    folders = []
+    Net::SFTP.start(host, username, password: password) do |sftp|
+      folders << "/Users/maxhoga/#{remote_path}"
+      files = get_files_recursively(sftp: sftp, folders: folders, files: files)
+    end
+
+    files
+  end
 end
