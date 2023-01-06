5a54d2420966357a74203f4dc1293c49ee3a0fcf
Михаил Дорохович <mihaildorohovic@192.168.1.8>
2023-01-07 01:49:23 +0300

    wip: updated query for removing schedules

--- graphql/app/jobs/remove_firm_job.rb
+++ graphql/app/jobs/remove_firm_job.rb
@@ -7,9 +7,10 @@ class RemoveFirmJob < ApplicationJob
     firm = Firm.find(firm_id)
     firm.events.each do |event|
       event.soft_delete!
-      Schedule.where(event_id: event.id).left_outer_joins(:bookings).each do |schedule|
-        schedule.destroy unless schedule.scheduled_for.to_datetime < Time.zone.now || Booking.where(schedule_id: schedule.id).count != 0
-      end
+      Schedule.where(event_id: event.id)
+              .left_outer_joins(:bookings)
+              .where('schedules.scheduled_for > ?', Time.zone.now)
+              .destroy_all
     end
   end
 end
