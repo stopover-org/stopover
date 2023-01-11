56c3831fc7d290bd82428159f4b477913b144259
maxhooga <51508706+maxhooga@users.noreply.github.com>
2023-01-11 18:10:40 +0000

    Draf: Firm mutation (#76)
    * wip: changed relations
    
    * fix: temporary test
    
    * wip: changed relations
    
    * wip: update firm

--- graphql/app/graphql/mutations/update_firm.rb
+++ graphql/app/graphql/mutations/update_firm.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 module Mutations
-  class CreateFirm < BaseMutation
+  class UpdateFirm < BaseMutation
     field :firm, Types::FirmType
 
     argument :city, String, required: false
     argument :contact_person, String, required: false
     argument :contacts, String, required: false
-    argument :country, String, required: false
     argument :description, String, required: false
     argument :full_address, String, required: false
     argument :house_number, String, required: false
@@ -22,19 +21,10 @@ module Mutations
     argument :website, String, required: false
 
     def resolve(**args)
-      raise GraphQL::ExecutionError, 'unauthorized' unless context[:current_user]
-      raise GraphQL::ExecutionError, 'user has no account' unless context[:current_user].account
-      raise GraphQL::ExecutionError, 'account already has a firm' if context[:current_user].account.firm
-
-      firm = Firm.new(args)
-
-      firm.accounts.push(context[:current_user].account)
-      firm.primary_email = context[:current_user].email if args[:primary_email].blank?
-      firm.primary_phone = context[:current_user].phone if args[:primary_phone].blank?
-
-      firm.save!
-
-      { firm: firm }
+      context[:current_user].account.firm.update!(**args)
+      {
+        firm: context[:current_user].account.firm
+      }
     end
   end
 end
