6c74b9fd8c03cec21a30ccbdad628bf0273d6de2
maxhooga <maxhooga@gmail.com>
2023-02-03 14:59:23 +0100

    wip: filter by tags done

--- graphql/app/queries/events_query.rb
+++ graphql/app/queries/events_query.rb
@@ -8,15 +8,20 @@ class EventsQuery
   end
 
   def all
-    if @params[:start_date] && @params[:end_date]
-      @relation = @relations.where('schedules.scheduled_for BETWEEN ? AND ?',
-                                   @params[:start_date],
-                                   @params[:end_date])
+    if @params[:start_date].present? && @params[:end_date].present?
+      @relation = @relations.joins(:schedules).where('schedules.scheduled_for BETWEEN ? AND ?',
+                                                     @params[:start_date],
+                                                     @params[:end_date])
     end
-    if @params[:min_price] && @params[:max_price]
+    if @params[:min_price].present? && @params[:max_price].present?
       @relations = @relations.where('attendee_price_per_uom_cents BETWEEN ? AND ?',
                                     @params[:min_price],
                                     @params[:max_price])
+    end
+    if @params[:tags].present?
+      @relations = @relations.joins(:tags).where(tags: {
+                                                   title: @params[:tags]
+                                                 })
     end
     @relations
   end
