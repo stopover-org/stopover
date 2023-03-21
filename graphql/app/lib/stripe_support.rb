9af6f82246fb6e88aec2052106526c49e6fb72b9
maxhooga <maxhooga@gmail.com>
2023-03-21 20:14:21 +0100

    wip: well played

--- graphql/app/lib/stripe_support.rb
+++ graphql/app/lib/stripe_support.rb
@@ -3,28 +3,44 @@
 class StripeSupport
   def self.generate_stripe_checkout_session(booking, payment_type)
     event_stripe_integration = booking.event.stripe_integrations.where(price_type: payment_type).first
-
-    event_options = booking.event_options
-    # TODO: add attendee options to checkout
-
     payment = Payment.create!(booking: booking)
+
+    attendee_options = {}
+    booking.attendees.map do |att|
+      att.attendee_options.each do |att_opt|
+        if attendee_options[att_opt.event_option.id].nil?
+          attendee_options[att_opt.event_option.id] = {
+            price: att_opt.event_option.stripe_integration.price_id,
+            quantity: 0
+          }
+        end
+
+        attendee_options[att_opt.event_option.id][:quantity] += 1
+      end
+    end
+
+    booking_options = booking.booking_options.map do |opt|
+      {
+        price: opt.event_option.stripe_integration.price_id,
+        quantity: 1
+      }
+    end
+    # TODO: add attendee options to checkout
+    # TODO: dont know how to covered by test
 
     checkout = Stripe::Checkout::Session.create({
                                                   line_items: [{
                                                     price: event_stripe_integration.price_id,
                                                                  quantity: booking.attendees.count
                                                   },
-                                                               *event_options.map do |opt|
-                                                                 {
-                                                                   price: opt.stripe_integration.price_id,
-                                                                   quantity: 1
-                                                                 }
-                                                               end],
+                                                               *booking_options,
+                                                               *attendee_options.values],
                                                   mode: 'payment',
                                                   success_url: "http://localhost:3000/checkouts/success/#{GraphqlSchema.id_from_object(payment)}",
                                                   cancel_url: "http://localhost:3000/checkouts/cancel/#{GraphqlSchema.id_from_object(payment)}",
-                                                  expires_at: 1_800
+                                                  expires_at: (Time.zone.now + 30.60).to_i
                                                 })
+
     payment.stripe_checkout_session_id = checkout[:id]
     payment.process!
     {
