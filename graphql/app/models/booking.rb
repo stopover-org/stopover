ea6b2d0e30a2ab1a79b22bcedaaf81158423940b
maxhooga <maxhooga@gmail.com>
2023-04-02 15:56:19 +0200

    fix: change request done

--- graphql/app/models/booking.rb
+++ graphql/app/models/booking.rb
@@ -77,7 +77,7 @@ class Booking < ApplicationRecord
   def attendee_total_price
     event_price = event.attendee_price_per_uom * attendees.count
 
-    booking_options_price = booking_options.sum(0) { |option| option.attendee_price }
+    booking_options_price = booking_options.sum(Money.new(0)) { |option| option.attendee_price }
     attendee_options_price = attendees.sum(Money.new(0)) do |att|
       res = Money.new(0)
       att.attendee_options.each do |att_opt|
@@ -92,7 +92,7 @@ class Booking < ApplicationRecord
 
   def organizer_total_price
     event_price = event.organizer_price_per_uom * attendees.count
-    booking_options_price = booking_options.sum(0) { |option| option.organizer_price }
+    booking_options_price = booking_options.sum(Money.new(0)) { |option| option.organizer_price }
     attendee_options_price = attendees.sum(Money.new(0)) do |att|
       res = Money.new(0)
       att.attendee_options.each do |att_opt|
