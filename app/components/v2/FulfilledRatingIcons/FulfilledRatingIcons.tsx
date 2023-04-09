367f5f1dd4fcdd1b733ea7084097728c0bb469d6
Mikhail Dorokhovich <mikhail@dorokhovich.ru>
2023-04-10 00:37:02 +0300

    wip: fix types issues

--- app/components/v2/FulfilledRatingIcons/FulfilledRatingIcons.tsx
+++ app/components/v2/FulfilledRatingIcons/FulfilledRatingIcons.tsx
@@ -9,35 +9,20 @@ const Item = styled(Sheet)(({ theme }) => ({
 
 interface BaseIconProps {
   ratingPercentage: string;
-  onMouseEnter?: (rating: number) => void;
-  onMouseLeave?: () => void;
-  onClick?: (rating: number) => void;
 }
 
 interface IconProps
   extends Omit<SheetProps, keyof BaseIconProps>,
     BaseIconProps {}
 
-const FulfilledIcons = ({
-  ratingPercentage,
-  onMouseEnter,
-  onMouseLeave,
-  onClick,
-  ...props
-}: IconProps) => (
+const FulfilledIcons = ({ ratingPercentage, ...props }: IconProps) => (
   <Stack
     direction="row"
     justifyContent="flex-start"
     sx={{ width: ratingPercentage, overflow: "hidden" }}
-    onMouseLeave={() => onMouseLeave}
   >
     {new Array(5).fill("").map((_, index) => (
-      <Item
-        key={`fulfilled-${index}`}
-        {...props}
-        onMouseEnter={() => onMouseEnter(index + 1)}
-        onClick={() => onClick(index + 1)}
-      >
+      <Item key={`fulfilled-${index}`} {...props}>
         <StarIcon color="primary" sx={{ width: "30px", height: "30px" }} />
       </Item>
     ))}
