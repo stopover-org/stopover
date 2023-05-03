a1e724cac614a30e647b534f113a84efedc919d5
Mikhail Dorokhovich <mikhail@dorokhovich.ru>
2023-05-03 16:42:14 +0300

    wip: updated node deps and fixed issue with removed user auth

--- graphql/app/controllers/graphql_controller.rb
+++ graphql/app/controllers/graphql_controller.rb
@@ -70,7 +70,7 @@ class GraphqlController < ApplicationController
     user = Stopover::AuthorizationSupport.decode_user(headers: request.headers, cookies: cookies)
     @current_user = user
   rescue StandardError => e
-    cookies.delete Stopover::AuthorizationSupport::COOKIE_KEY if e.is_a? JWT::VerificationError
+    cookies.delete Stopover::AuthorizationSupport::COOKIE_KEY if e.is_a?(JWT::VerificationError) || e.is_a?(ActiveRecord::RecordNotFound)
     raise e unless Rails.env.development?
 
     handle_error_in_development(e)
