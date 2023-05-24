ab775f08cafcdc37b76b98d21f7daeae0bcc423b
Mikhail Dorokhovich <mikhail@dorokhovich.ru>
2023-05-24 09:17:21 +0200

    wip: some improvements

--- app/scenes/CreateFirmScene/CreateFirmScene.tsx
+++ app/scenes/CreateFirmScene/CreateFirmScene.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AspectRatio, Box, Grid } from "@mui/joy";
+import { AspectRatio, Box, Card, CardContent, Grid } from "@mui/joy";
 import ClearIcon from "@mui/icons-material/Clear";
 import Input from "../../components/v2/Input";
 import Typography from "../../components/v2/Typography";
@@ -7,121 +7,169 @@ import Button from "../../components/v2/Button";
 import { useCreateFirmForm } from "./useCreateFirmForm";
 import PhoneInput from "../../components/v2/PhoneInput";
 import FileUploader from "../../components/v2/FileUploader";
+import TextArea from "../../components/v2/TextArea";
 
 const CreateFirmScene = () => {
   const form = useCreateFirmForm();
   const imagesField = form.useFormField("image");
   return (
     <form onSubmit={form.handleSubmit()}>
-      <Grid container spacing={2} padding={2}>
-        <Grid xs={12}>
-          <Typography level="h3">Firm title</Typography>
-        </Grid>
-        <Grid md={6} sm={12}>
-          <Input {...form.useFormField("title")} label="Title" />
-        </Grid>
-        <Grid md={6} sm={12}>
-          <Input
-            {...form.useFormField("contactPerson")}
-            label="Contact Person"
-          />
-        </Grid>
-        <Grid xs={12}>
-          <FileUploader
-            onChange={(images) => imagesField.onChange(images[0])}
-            maxFiles={1}
-          />
-        </Grid>
-        {imagesField.value && (
-          <Grid xs={12}>
-            <AspectRatio
-              variant="outlined"
-              ratio="4/3"
-              sx={{
-                width: 300,
-                bgcolor: "background.level2",
-                borderRadius: "md",
-                position: "relative",
-              }}
-            >
-              <img alt="Logo Preview" src={imagesField.value} />
+      <Grid container spacing={2} md={8} sm={12}>
+        <Card sx={{ margin: "20px" }} variant="outlined">
+          <CardContent>
+            <Grid container spacing={2}>
+              <Grid xs={12}>
+                <Typography level="h3">Firm title</Typography>
+              </Grid>
+              <Grid md={6} sm={12}>
+                <Input {...form.useFormField("title")} label="Title" />
+              </Grid>
+              <Grid md={6} sm={12}>
+                <Input
+                  {...form.useFormField("contactPerson")}
+                  label="Contact Person"
+                />
+              </Grid>
+              <Grid xs={12}>
+                <FileUploader
+                  onChange={(images) => imagesField.onChange(images[0])}
+                  maxFiles={1}
+                />
+              </Grid>
+              {imagesField.value && (
+                <Grid xs={12}>
+                  <AspectRatio
+                    variant="outlined"
+                    ratio="4/3"
+                    sx={{
+                      width: 300,
+                      bgcolor: "background.level2",
+                      borderRadius: "md",
+                      position: "relative",
+                    }}
+                  >
+                    <img alt="Logo Preview" src={imagesField.value} />
 
-              <Box
-                sx={{
-                  position: "absolute",
-                  zIndex: 2,
-                  right: "1rem",
-                  top: "1rem",
-                  borderRadius: "50%",
-                  backgroundColor: "white",
-                  width: "30px",
-                  height: "30px",
-                  padding: "5px",
-                  cursor: "pointer",
-                }}
-                onClick={() => imagesField.onChange("")}
-              >
-                <ClearIcon sx={{ color: "black" }} />
-              </Box>
-            </AspectRatio>
-          </Grid>
-        )}
+                    <Box
+                      sx={{
+                        position: "absolute",
+                        zIndex: 2,
+                        right: "1rem",
+                        top: "1rem",
+                        borderRadius: "50%",
+                        backgroundColor: "white",
+                        width: "30px",
+                        height: "30px",
+                        padding: "5px",
+                        cursor: "pointer",
+                      }}
+                      onClick={() => imagesField.onChange("")}
+                    >
+                      <ClearIcon sx={{ color: "black" }} />
+                    </Box>
+                  </AspectRatio>
+                </Grid>
+              )}
+            </Grid>
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid container spacing={2} md={8} sm={12}>
+        <Card sx={{ margin: "20px" }} variant="outlined">
+          <CardContent>
+            <Grid container padding={2}>
+              <Grid xs={12}>
+                <Typography level="h3">Address</Typography>
+              </Grid>
+              <Grid md={6} sm={12}>
+                <Input {...form.useFormField("country")} label="Country" />
+              </Grid>
+              <Grid md={6} sm={12}>
+                <Input {...form.useFormField("region")} label="Region" />
+              </Grid>
+              <Grid md={6} sm={12}>
+                <Input {...form.useFormField("city")} label="City" />
+              </Grid>
+              <Grid md={6} sm={12}>
+                <Input {...form.useFormField("street")} label="Street" />
+              </Grid>
+              <Grid md={6} sm={12}>
+                <Input
+                  {...form.useFormField("houseNumber")}
+                  label="House Number"
+                />
+              </Grid>
+              <Grid xs={12}>
+                <Input
+                  {...form.useFormField("fullAddress")}
+                  label="Full Address"
+                  hint="<lattitude>, <longitude>"
+                />
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+      </Grid>
 
-        <Grid xs={12} pt={5}>
-          <Typography level="h3">Address</Typography>
-        </Grid>
-        <Grid md={6} sm={12}>
-          <Input {...form.useFormField("country")} label="Country" />
-        </Grid>
-        <Grid md={6} sm={12}>
-          <Input {...form.useFormField("region")} label="Region" />
-        </Grid>
-        <Grid md={6} sm={12}>
-          <Input {...form.useFormField("city")} label="City" />
-        </Grid>
-        <Grid md={6} sm={12}>
-          <Input {...form.useFormField("street")} label="Street" />
-        </Grid>
-        <Grid md={6} sm={12}>
-          <Input {...form.useFormField("houseNumber")} label="House Number" />
-        </Grid>
-        <Grid xs={12}>
-          <Input
-            {...form.useFormField("fullAddress")}
-            label="Full Address"
-            hint="<lattitude>, <longitude>"
-          />
-        </Grid>
+      <Grid container spacing={2} md={8} sm={12}>
+        <Card sx={{ margin: "20px" }} variant="outlined">
+          <CardContent>
+            <Grid container padding={2}>
+              <Grid xs={12}>
+                <Typography level="h3">Contact Information</Typography>
+              </Grid>
+              <Grid md={6} sm={12}>
+                <Input
+                  {...form.useFormField("primaryEmail")}
+                  label="Primary Email"
+                />
+              </Grid>
+              <Grid md={6} sm={12}>
+                <PhoneInput
+                  {...form.useFormField("primaryPhone")}
+                  label="Primary Phone"
+                />
+              </Grid>
+              <Grid xs={12}>
+                <Input {...form.useFormField("contacts")} label="Contacts" />
+              </Grid>
+              <Grid xs={12}>
+                <Input {...form.useFormField("website")} label="Website" />
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+      </Grid>
 
-        <Grid xs={12} pt={5}>
-          <Typography level="h3">Contact Information</Typography>
-        </Grid>
-        <Grid md={6} sm={12}>
-          <Input {...form.useFormField("primaryEmail")} label="Primary Email" />
-        </Grid>
-        <Grid md={6} sm={12}>
-          <PhoneInput
-            {...form.useFormField("primaryPhone")}
-            label="Primary Phone"
-          />
-        </Grid>
-        <Grid xs={12}>
-          <Input {...form.useFormField("contacts")} label="Contacts" />
-        </Grid>
-        <Grid xs={12}>
-          <Input {...form.useFormField("website")} label="Website" />
-        </Grid>
+      <Grid container spacing={2} md={8} sm={12}>
+        <Card sx={{ margin: "20px" }} variant="outlined">
+          <CardContent>
+            <Grid container padding={2}>
+              <Grid xs={12}>
+                <Typography level="h3">Description</Typography>
+              </Grid>
+              <Grid xs={12}>
+                <TextArea
+                  {...form.useFormField("description")}
+                  label="Description"
+                  minRows={5}
+                />
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+      </Grid>
 
-        <Grid xs={12} pt={5}>
-          <Typography level="h3">Description</Typography>
-        </Grid>
-        <Grid xs={12}>
-          <Input {...form.useFormField("description")} label="Description" />
-        </Grid>
-
-        <Grid md={6} sm={12}>
-          <Button type="submit">Submit</Button>
-        </Grid>
+      <Grid container spacing={2} md={8} sm={12}>
+        <Card sx={{ margin: "20px" }} variant="outlined">
+          <CardContent>
+            <Grid container padding={2}>
+              <Grid xs={12}>
+                <Button type="submit">Submit</Button>
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
       </Grid>
     </form>
   );
--- graphql/spec/factories/account_firm_factory.rb
+++ graphql/spec/factories/account_firm_factory.rb
@@ -1,5 +1,25 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: account_firms
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  account_id :bigint
+#  firm_id    :bigint
+#
+# Indexes
+#
+#  index_account_firms_on_account_id  (account_id)
+#  index_account_firms_on_firm_id     (firm_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (account_id => accounts.id)
+#  fk_rails_...  (firm_id => firms.id)
+#
 FactoryBot.define do
   factory :account_firm do
     account { create(:account) }
