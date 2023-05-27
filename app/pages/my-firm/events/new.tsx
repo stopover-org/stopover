22d51f39f3625e042dca0f4214ce157643755661
maxhooga <51508706+maxhooga@users.noreply.github.com>
2023-05-27 08:37:19 +0100

    Create event (#141)
    * wip: sidebar for my firm done
    
    * fix: fixed container
    
    * wip: started page create event
    
    * wip: added required inputs
    
    * wip: sidebar for my firm done
    
    * fix: fixed container
    
    * wip: what is done is done

---
+++ app/pages/my-firm/events/new.tsx
@@ -0,0 +1,44 @@
+import { RelayProps, withRelay } from "relay-nextjs";
+import React from "react";
+import { graphql, usePreloadedQuery } from "react-relay";
+import CreateEventScene from "../../../scenes/CreateEventScene";
+import { getClientEnvironment } from "../../../lib/clientEnvironment";
+import Layout from "../../../components/MainPage/Layout";
+import { new_FirmEventsNewQuery } from "./__generated__/new_FirmEventsNewQuery.graphql";
+
+const Query = graphql`
+  query new_FirmEventsNewQuery {
+    currentUser {
+      ...Layout_CurrentUserFragment
+    }
+  }
+`;
+
+const New = ({ preloadedQuery }: RelayProps<{}, new_FirmEventsNewQuery>) => {
+  const data = usePreloadedQuery(Query, preloadedQuery);
+  return (
+    <Layout currentUserFragment={data.currentUser!}>
+      <CreateEventScene />
+    </Layout>
+  );
+};
+
+export default withRelay(New, Query, {
+  // Fallback to render while the page is loading.
+  // This property is optional.
+  fallback: null,
+  // Create a Relay environment on the client-side.
+  // Note: This function must always return the same value.
+  createClientEnvironment: () => getClientEnvironment()!,
+  // Gets server side props for the page.
+  serverSideProps: async () => ({}),
+  // Server-side props can be accessed as the second argument
+  // to this function.
+  createServerEnvironment: async ({ req }) => {
+    const { createServerEnvironment } = await import(
+      "../../../lib/serverEnvironment"
+    );
+
+    return createServerEnvironment(req!.headers.cookie!);
+  },
+});
---
+++ app/scenes/CreateEventScene/useCreateEventForm.ts
@@ -0,0 +1,99 @@
+import React from "react";
+import * as Yup from "yup";
+import { graphql } from "react-relay";
+import { yupResolver } from "@hookform/resolvers/yup";
+import useMutationForm from "../../lib/hooks/useMutationForm";
+
+interface CreateEventFields {
+  title: string;
+  description: string;
+  houseNumber: string;
+  street: string;
+  city: string;
+  country: string;
+  region: string;
+  fullAddress: string;
+  durationTime: string;
+  eventType: string;
+  maxAttendees: number;
+  minAttendees: number;
+  organizerPricePerUomCents: number;
+  recurringType: string;
+  requiresCheckIn: boolean;
+  requiresContract: boolean;
+  requiresPassport: boolean;
+  requiresPrepaid: boolean;
+}
+
+function useDefaultValues(): CreateEventFields {
+  return React.useMemo(
+    () => ({
+      title: "",
+      description: "",
+      houseNumber: "",
+      street: "",
+      city: "",
+      country: "",
+      region: "",
+      fullAddress: "",
+      durationTime: "",
+      eventType: "excursion",
+      maxAttendees: 0,
+      minAttendees: 0,
+      organizerPricePerUomCents: 0,
+      recurringType: "regular",
+      requiresCheckIn: false,
+      requiresContract: false,
+      requiresPassport: false,
+      requiresPrepaid: false,
+    }),
+    []
+  );
+}
+
+const validationSchema = Yup.object().shape({
+  title: Yup.string().required(),
+  description: Yup.string(),
+  houseNumber: Yup.string(),
+  street: Yup.string(),
+  city: Yup.string(),
+  country: Yup.string(),
+  region: Yup.string(),
+  fullAddress: Yup.string(),
+  durationTime: Yup.string(),
+  eventType: Yup.string(),
+  maxAttendees: Yup.number(),
+  minAttendees: Yup.number(),
+  organizerPricePerUomCents: Yup.number(),
+  recurringType: Yup.string(),
+  requiresCheckIn: Yup.boolean(),
+  requiresContract: Yup.boolean(),
+  requiresPassport: Yup.boolean(),
+  requiresPrepaid: Yup.boolean(),
+});
+
+export function useCreateEventForm() {
+  return useMutationForm(
+    graphql`
+      mutation useCreateEventForm_CreateEventMutation(
+        $input: CreateEventInput!
+      ) {
+        createEvent(input: $input) {
+          event {
+            id
+          }
+        }
+      }
+    `,
+    ({ image, ...values }) => ({
+      input: {
+        ...values,
+        base64Image: image,
+      },
+    }),
+    {
+      defaultValues: useDefaultValues(),
+      resolver: yupResolver(validationSchema),
+    }
+  );
+}
