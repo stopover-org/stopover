11594ee4dff28373a865933fd226a8bc6ed0f306
maxhooga <51508706+maxhooga@users.noreply.github.com>
2023-06-22 15:01:06 +0100

    Verify event (#158)
    * wip: added tag with status for event
    
    * wip: service user submit button started
    
    * wip: verify event done

---
+++ graphql/app/graphql/mutations/verify_event.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Mutations
+  class VerifyEvent < BaseMutation
+    field :event, Types::EventType
+
+    argument :event_id, ID, loads: Types::EventType
+
+    def resolve(event:, **_args)
+      raise GraphQL::ExecutionError, "You don't have rights to do it" unless context[:current_user].service_user
+      event.publish!
+
+      {
+        event: event
+      }
+    end
+  end
+end
