c4c47e73f5beba15f23251a2ecc6e25b9078ada0
Mikhail Dorokhovich <mikhail@dorokhovich.ru>
2023-07-17 20:53:59 +0200

    wip: disabled graphiql for dev

--- graphql/config/routes.rb
+++ graphql/config/routes.rb
@@ -56,7 +56,7 @@
 #        GET  /           graphiql/rails/editors#show
 
 Rails.application.routes.draw do
-  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
+  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
 
   post '/graphql', to: 'graphql#execute'
 
