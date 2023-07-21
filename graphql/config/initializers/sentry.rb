7ce5cdb07240dc16c3d9b7235cda606347554448
Mikhail Dorokhovich <mikhail@dorokhovich.ru>
2023-07-22 01:12:04 +0200

    wip: moved sentry settings to credentials

--- graphql/config/initializers/sentry.rb
+++ graphql/config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
-    config.dsn = 'https://7d3942c461ca408d94118dd25518f6bb@o4505569844723712.ingest.sentry.io/4505569846951936'
+    config.dsn = Rails.application.credentials.sentry_sdn
     config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
     # Set traces_sample_rate to 1.0 to capture 100%
