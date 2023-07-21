5b2707e79680edadc4ff35dc194fbd0c76f7e950
Mikhail Dorokhovich <mikhail@dorokhovich.ru>
2023-07-21 23:23:31 +0200

    wip: creds issue

--- graphql/config/deploy.rb
+++ graphql/config/deploy.rb
@@ -25,11 +25,11 @@ set :branch, 'main'
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", 'config/master.key'
-set :linked_files, %w[config/application.yml config/database.yml config/master.key]
+set :linked_files, %w[config/credentials/production.key config/application.yml config/database.yml config/master.key]
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
-set :linked_dirs, %w[config/credentials log tmp/pids tmp/cache tmp/sockets vendor/bundle .bundle public/system public/uploads node_modules]
+set :linked_dirs, %w[log tmp/pids tmp/cache tmp/sockets vendor/bundle .bundle public/system public/uploads node_modules]
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
