82787d4a647016eaa16da8b9cb736fbae180e592
Mikhail Dorokhovich <mikhail@dorokhovich.ru>
2023-07-22 18:48:17 +0200

    wip: fixed env issue

--- graphql/config/initializers/sentry.rb
+++ graphql/config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
-    config.dsn = Rails.application.credentials.sentry_sdn
+    config.dsn = Rails.application.credentials.sentry_dsn
     config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
     # Set traces_sample_rate to 1.0 to capture 100%
