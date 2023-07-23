1193d2d9dc7eaf3c888445234a5bb6e2a58642a9
Mikhail Dorokhovich <mikhail@dorokhovich.ru>
2023-07-23 02:56:52 +0200

    wip: small fix

--- graphql/config/initializers/cors.rb
+++ graphql/config/initializers/cors.rb
@@ -2,11 +2,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'stopover.app.dorokhovich.ru'
+    origins 'http://localhost:3000', 'http://stopover.app.dorokhovich.ru'
     resource '*',
              headers: :any,
              methods: %i[get post patch put],
-             expose: ['X-Total-Count'],
              credentials: true
   end
 end
