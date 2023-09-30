d0c03f25e97fd8f09e0123f636205947785a7280
Mikhail Dorokhovich <mikhail@dorokhovich.ru>
2023-09-30 16:54:44 +0200

    wip: intoroduced translations for all gql types

--- app/config/locales/ru/models/users_related/user.ts
+++ app/config/locales/ru/models/users_related/user.ts
@@ -2,7 +2,12 @@ export default {
   models: {
     user: {
       attributes: {
-
+        id: 'ID',
+        email: 'Email',
+        phone: 'Телефон',
+        status: 'Статус',
+        account: 'Аккаунт',
+        serviceUser: 'Сервисный пользователь',
       }
     }
   }
