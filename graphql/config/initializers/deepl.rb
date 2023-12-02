f1bf7b1ba6fd5c2b1d1f6b059e432841eed35998
Mikhail Dorokhovich <mikhail@dorokhovich.ru>
2023-12-02 22:24:19 +0100

    fix: fix deepl

--- graphql/config/initializers/deepl.rb
+++ graphql/config/initializers/deepl.rb
@@ -1,4 +1,6 @@
-DeepL.configure do |config|
-  config.auth_key = Rails.application.credentials.deepl_key || ''
-  config.host = 'https://api-free.deepl.com'
+unless Rails.env.test?
+  DeepL.configure do |config|
+    config.auth_key = Rails.application.credentials.deepl_key || ''
+    config.host = 'https://api-free.deepl.com'
+  end
 end
