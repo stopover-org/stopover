d13549340e72dee9a9f70059b80357eafb3e4cc2
Dorokhovich Michael <mikhail@dorokhovich.ru>
2023-12-10 21:39:06 +0100

    wip: improved docker compose

--- graphql/config/initializers/searchkick.rb
+++ graphql/config/initializers/searchkick.rb
@@ -1,9 +1,1 @@
 ENV['OPENSEARCH_URL'] = Rails.application.credentials.opensearch_url || 'http://localhost:9200'
-
-if Rails.env.production?
-  Searchkick.aws_credentials = {
-    access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
-    secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-    region: 'eu-central-1'
-  }
-end
