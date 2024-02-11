82226562cff40b9ee156eb309c6ebd5f05d0b697
Dorokhovich Michael <mikhail@dorokhovich.ru>
2024-02-11 11:39:59 +0100

    wip: changed CTA

--- app/components/shared/RegisterFirmCTA/RegisterFirmCTA.tsx
+++ app/components/shared/RegisterFirmCTA/RegisterFirmCTA.tsx
@@ -1,23 +1,25 @@
-import { AspectRatio, Box, Stack, useTheme } from "@mui/joy";
+import {
+  AspectRatio,
+  Box,
+  Card,
+  CardContent,
+  CardOverflow,
+  useTheme,
+} from "@mui/joy";
 import React from "react";
 import { useMediaQuery } from "@mui/material";
 import { useTranslation } from "react-i18next";
-import Link from "components/v2/Link/Link";
+import Link from "components/v2/Link";
 
 export const RegisterFirmCTA = () => {
-  const [isServer, setIsServer] = React.useState<boolean>(true);
   const theme = useTheme();
   const isMobileView = useMediaQuery(theme.breakpoints.down("md"));
   const { t } = useTranslation();
 
-  React.useEffect(() => {
-    setIsServer(false);
-  });
-
   return (
     <Box
-      width={isMobileView ? "100%" : "500px"}
-      height={isMobileView ? "unset" : "500px"}
+      width={isMobileView ? "100%" : "400px"}
+      height={isMobileView ? "unset" : "400px"}
       margin="0 auto"
       sx={{
         borderRadius: "3px",
@@ -25,46 +27,23 @@ export const RegisterFirmCTA = () => {
       }}
       position="relative"
     >
-      <AspectRatio minHeight="100%" maxHeight="100%">
-        <img
-          src="https://s3.eu-north-1.amazonaws.com/stopoverx.production/portrait-flight-attendant-with-inflatable-jacket_23-2150282858.jpg"
-          srcSet="https://s3.eu-north-1.amazonaws.com/stopoverx.production/portrait-flight-attendant-with-inflatable-jacket_23-2150282858.jpg 2x"
-          loading="lazy"
-          alt=""
-        />
-      </AspectRatio>
-      {!isServer && (
-        <Link
-          href="/firms/new"
-          position="absolute"
-          sx={{
-            transform: "translate(-50%, -75%)",
-            top: "75%",
-            left: "50%",
-            color: "white",
-            opacity: 0.95,
-            transition: "opacity 0.1s ease 0s",
-            "&:hover": {
-              opacity: 1,
-            },
-          }}
-          underline={false}
-          fontSize="18px"
-        >
-          <Stack
-            direction="row"
-            alignItems="center"
-            justifyContent="center"
-            width="250px"
-            height="200px"
-            sx={{
-              background: `url(https://s3.eu-north-1.amazonaws.com/stopoverx.production/Group+1.svg) center center/cover no-repeat`,
-            }}
-          >
+      <Card variant="outlined">
+        <CardOverflow>
+          <AspectRatio minHeight="100%" maxHeight="100%">
+            <img
+              src="https://s3.eu-north-1.amazonaws.com/stopoverx.production/lifebelt-4152728_1280.jpg"
+              srcSet="https://s3.eu-north-1.amazonaws.com/stopoverx.production/lifebelt-4152728_1280.jpg 2x"
+              loading="lazy"
+              alt={t("scenes.rootScene.becomePartner")}
+            />
+          </AspectRatio>
+        </CardOverflow>
+        <CardContent orientation="horizontal">
+          <Link href="/firms/new" fontSize="lg" primary>
             {t("scenes.rootScene.becomePartner")}
-          </Stack>
-        </Link>
-      )}
+          </Link>
+        </CardContent>
+      </Card>
     </Box>
   );
 };
