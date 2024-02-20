60aea1ccff1b355f6338bfb04142706fda9e7075
Dorokhovich Michael <mikhail@dorokhovich.ru>
2024-02-20 17:41:59 +0100

    wip: up client pages

--- app/app/pages/privacy/page.tsx
+++ app/app/pages/privacy/page.tsx
@@ -3,9 +3,6 @@
 import PageWrapper from "components/shared/PageWrapper";
 import SceneWrapper from "components/shared/SceneWrapper";
 import React from "react";
-import { Metadata } from "next";
-import { merge } from "lodash";
-import defaultMetadata, { translate } from "lib/utils/defaultMetadata";
 import Privacy from "./scene";
 
 export default () => (
@@ -17,13 +14,3 @@ export default () => (
     </SceneWrapper>
   </PageWrapper>
 );
-
-export const generateMetadata = async (): Promise<Metadata> => {
-  const title = await translate("general.privacy");
-  return merge(defaultMetadata, {
-    title,
-    openGraph: {
-      title,
-    },
-  });
-};
