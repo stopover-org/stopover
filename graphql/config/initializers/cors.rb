75aa595ae94a76816259f4393e066bc488287e1c
Dorokhovich Michael <mikhail@dorokhovich.ru>
2024-02-21 23:50:42 +0100

    wip: upd cors

--- graphql/config/initializers/cors.rb
+++ graphql/config/initializers/cors.rb
@@ -2,7 +2,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'http://staging.stopoverx.com', 'http://local.stopoverx.test'
+    origins 'http://localhost:3000', 'http://staging.stopoverx.com',
+            'http://local.stopoverx.test', 'https://staging.stopoverx.com',
+            'https://local.stopoverx.test', 'http://stopoverx.com',
+            'https://stopoverx.com'
     resource '*',
              headers: :any,
              methods: %i[get post patch put],
