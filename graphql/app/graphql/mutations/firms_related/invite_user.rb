e3d9f8fbaf7afa34858f81c68813973d31a68445
Dorokhovich Michael <mikhail@dorokhovich.ru>
2024-02-23 12:41:57 +0100

    wip: upd spec

--- graphql/app/graphql/mutations/firms_related/invite_user.rb
+++ graphql/app/graphql/mutations/firms_related/invite_user.rb
@@ -13,13 +13,42 @@ module Mutations
         user = User.find_by(email: args[:email].downcase)
 
         unless user
-          user = User.create!(email: args[:email])
+          user = User.create!(email: args[:email], status: :active)
           user.account = Account.create(user: user)
         end
+
+        user.update!(status: :active) if user.temporary?
 
         return { account: nil, errors: [I18n.t('graphql.errors.user_already_invited')] } if user.account.account_firms.where(firm: firm).count.positive?
 
         user.account.account_firms.create!(firm: firm)
+        user.account.update!(firm: firm)
+
+        Notification.create!(
+          delivery_method: 'email',
+          to: user.email,
+          subject: "You invited to #{firm.title}",
+          content: Stopover::MailProvider.prepare_content(
+            file: 'mailer/auth/invitation',
+            locals: {
+              firm: firm,
+              user: user
+            }
+          )
+        )
+
+        Notification.create!(
+          delivery_method: 'email',
+          to: firm.primary_email,
+          subject: "You invited #{user.email}",
+          content: Stopover::MailProvider.prepare_content(
+            file: 'mailer/firms/user_invited',
+            locals: {
+              firm: firm,
+              user: user
+            }
+          )
+        )
 
         { account: user.account, notification: I18n.t('graphql.mutations.invite_user.notifications.success') }
       rescue StandardError => e
