6a3c7f0d6735c82fec8a56e7736d29c25ea4ea87
Dorokhovich Michael <mikhail@dorokhovich.ru>
2024-04-06 10:39:11 +0200

    wip: wip

--- graphql/app/controllers/testings_controller.rb
+++ graphql/app/controllers/testings_controller.rb
@@ -4,7 +4,31 @@ require 'factory_bot'
 require 'factory_bot_rails'
 
 class TestingsController < ApplicationController
+  def setup_fixtures
+    raise 'Wrong Environment' unless Rails.env.test?
+    Rails.logger.debug 'load fixtures'
+    fixtures_dir = File.join(Rails.root, '/test/fixtures')
+    Dir.glob(File.join(fixtures_dir,'*.yml')).each do |file|
+      base_name = File.basename(file, '.*')
+      Rails.logger.debug "Loading #{base_name}..."
+      ActiveRecord::FixtureSet.create_fixtures(fixtures_dir, base_name)
+    end
+  end
+
+  def teardown_fixtures
+    raise 'Wrong Environment' unless Rails.env.test?
+    Rails.logger.debug 'Teardown'
+
+    ActiveRecord::Base.connection.disable_referential_integrity do
+      ApplicationRecord.descendants.each do |model|
+        model.delete_all
+      end
+    end
+  end
+
   def setup
+    raise 'Wrong Environment' unless Rails.env.test?
+
     result = []
     setup_variables = params[:setup_variables]
     setup_variables.each do |model_variable|
@@ -26,6 +50,8 @@ class TestingsController < ApplicationController
   end
 
   def teardown
+    raise 'Wrong Environment' unless Rails.env.test?
+
     Redis.new.lrange('testing:e2e:records', 0, -1).each do |record_hash|
       hash = JSON.parse(record_hash).deep_symbolize_keys
       model = hash[:model].camelize.constantize
