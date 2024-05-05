83ceb1947636bdd21376f2a71cb327de185ec08b
Dorokhovich Michael <mikhail@dorokhovich.ru>
2024-05-05 18:29:45 +0200

    wip: upd

--- graphql/app/lib/stopover/flags_support.rb
+++ graphql/app/lib/stopover/flags_support.rb
@@ -2,7 +2,6 @@
 
 module Stopover
   class FlagsSupport
-    # @param [TrueClass] skip
     def self.skip_notifications(skip: true)
       return unless skip
 
@@ -20,6 +19,19 @@ module Stopover
       return unless skip
 
       flag = :skip_stripe_integration
+      initial_disabled = !Flipper.enabled?(flag)
+      Flipper.enable(flag)
+      yield
+      Flipper.disable(flag) if initial_disabled
+    rescue StandardError => e
+      Flipper.disable(flag) if initial_disabled
+      raise e
+    end
+
+    def self.disable_phone_validation(skip: true)
+      return unless skip
+
+      flag = :skip_phone_validation
       initial_disabled = !Flipper.enabled?(flag)
       Flipper.enable(flag)
       yield
