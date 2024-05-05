e9fdad477375b01d4474c87ff4b22961333d5cf3
Dorokhovich Michael <mikhail@dorokhovich.ru>
2024-05-05 19:07:10 +0200

    wip: a bit refactoring

--- graphql/app/lib/stopover/flags_support.rb
+++ graphql/app/lib/stopover/flags_support.rb
@@ -6,39 +6,35 @@ module Stopover
       return unless skip
 
       flag = :skip_notifications_delivery
-      initial_disabled = !Flipper.enabled?(flag)
-      Flipper.enable(flag)
-      yield
-      Flipper.disable(flag) if initial_disabled
-    rescue StandardError => e
-      Flipper.disable(flag) if initial_disabled
-      raise e
+      disable_flag(flag)
     end
 
     def self.skip_stripe_integrations(skip: true)
       return unless skip
 
       flag = :skip_stripe_integration
-      initial_disabled = !Flipper.enabled?(flag)
-      Flipper.enable(flag)
-      yield
-      Flipper.disable(flag) if initial_disabled
-    rescue StandardError => e
-      Flipper.disable(flag) if initial_disabled
-      raise e
+      disable_flag(flag)
     end
 
     def self.disable_phone_validation(skip: true)
       return unless skip
 
       flag = :skip_phone_validation
-      initial_disabled = !Flipper.enabled?(flag)
-      Flipper.enable(flag)
-      yield
-      Flipper.disable(flag) if initial_disabled
-    rescue StandardError => e
-      Flipper.disable(flag) if initial_disabled
-      raise e
+      disable_flag(flag)
+    end
+
+    class << self
+      private
+
+      def disable_flag(flag)
+        initial_disabled = !Flipper.enabled?(flag)
+        Flipper.enable(flag)
+        yield
+        Flipper.disable(flag) if initial_disabled
+      rescue StandardError => e
+        Flipper.disable(flag) if initial_disabled
+        raise e
+      end
     end
   end
 end
