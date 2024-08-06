280b89c0dc4f6bd39096a5987b04d1247e75453c
Dorokhovich Michael <mikhail@dorokhovich.ru>
2024-08-06 23:18:12 +0200

    FEAT: added auth for data compositor ui - Mikhail Dorokhovich

---
+++ data-compositor-ui/src/components/Logout.tsx
@@ -0,0 +1,8 @@
+import { memo } from "react";
+import { signOut } from "next-auth/react";
+
+const Logout = () => (
+  <button onClick={() => signOut()}>Signout of keycloak</button>
+);
+
+export default memo(Logout);
