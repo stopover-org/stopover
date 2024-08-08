917215fb1bbf21b0bef9dff51c7a34b067fd2100
Dorokhovich Michael <mikhail@dorokhovich.ru>
2024-08-08 19:49:42 +0200

    FIX: logout - Mikhail Dorokhovich

--- data-compositor-ui/src/app/api/auth/[...nextauth]/route.ts
+++ data-compositor-ui/src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,9 @@
 import NextAuth, { AuthOptions } from "next-auth";
-import KeycloakProvider from "next-auth/providers/keycloak";
+import KeycloakProvider, {
+  KeycloakProfile,
+} from "next-auth/providers/keycloak";
+import { OAuthConfig } from "next-auth/providers/oauth";
+import { JWT } from "next-auth/jwt";
 
 export const authOptions: AuthOptions = {
   providers: [
@@ -9,6 +13,38 @@ export const authOptions: AuthOptions = {
       issuer: process.env.KEYCLOAK_ISSUER!,
     }),
   ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.id_token = account.id_token;
+
+        token.provider = account.provider;
+      }
+      return token;
+    },
+  },
+  events: {
+    async signOut({ token }: { token: JWT }) {
+      if (token.provider === "keycloak") {
+        const issuerUrl = (
+          authOptions.providers.find(
+            (p) => p.id === "keycloak",
+          ) as OAuthConfig<KeycloakProfile>
+        ).options!.issuer!;
+
+        const logOutUrl = new URL(
+          `${issuerUrl}/protocol/openid-connect/logout`,
+        );
+
+        logOutUrl.searchParams.set(
+          "id_token_hint",
+          (token as Record<string, any>)?.id_token!,
+        );
+
+        await fetch(logOutUrl);
+      }
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
