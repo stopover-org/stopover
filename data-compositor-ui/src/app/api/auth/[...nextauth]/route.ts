d53a1bdf820a2b474454dd3c84117a31234a7e60
Mihail <mikhail@dorokhovich.ru>
2024-08-09 11:35:58 +0200

    FEAT: simplified keycloak theme - Mikhail Dorokhovich (#276)
    * FEAT: simplified keycloak theme - Mikhail Dorokhovich
    
    * FIX: restored docker compose - Mikhail Dorokhovich
    
    * FIX: docker compose - Mikhail Dorokhovich
    
    * FIX: logout - Mikhail Dorokhovich
    
    * FIX: types - Mikhail Dorokhovich
    
    * FEAT: added roles validation - Mikhail Dorokhovich

--- data-compositor-ui/src/app/api/auth/[...nextauth]/route.ts
+++ data-compositor-ui/src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,33 @@
 import NextAuth, { AuthOptions } from "next-auth";
-import KeycloakProvider from "next-auth/providers/keycloak";
+import KeycloakProvider, {
+  KeycloakProfile,
+} from "next-auth/providers/keycloak";
+import { OAuthConfig } from "next-auth/providers/oauth";
+import { signOut } from "next-auth/react";
+import { JWT } from "next-auth/jwt";
+
+function parseJwt(token?: string) {
+  if (!token) {
+    return null;
+  }
+  return JSON.parse(Buffer.from(token.split(".")[1], "base64").toString());
+}
+
+async function signOutKeycloak(token: JWT) {
+  const issuerUrl =
+    // prettier-ignore
+    (
+      // eslint-disable-next-line @typescript-eslint/no-use-before-define
+      authOptions.providers.find(
+        (p) => p.id === "keycloak",
+      ) as OAuthConfig<KeycloakProfile>
+    ).options!.issuer!;
+  const logOutUrl = new URL(`${issuerUrl}/protocol/openid-connect/logout`);
+
+  logOutUrl.searchParams.set("id_token_hint", token.id_token as string);
+
+  await fetch(logOutUrl);
+}
 
 export const authOptions: AuthOptions = {
   providers: [
@@ -9,6 +37,38 @@ export const authOptions: AuthOptions = {
       issuer: process.env.KEYCLOAK_ISSUER!,
     }),
   ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.id_token = account.id_token;
+
+        token.provider = account.provider;
+      }
+      const additional = parseJwt(token.id_token as string) || {};
+
+      token = { ...additional, ...token };
+
+      if (
+        !(token.groups as string[])
+          .map((group: string) => group.toLowerCase())
+          .includes("/analyst")
+      ) {
+        await signOutKeycloak(token);
+
+        await signOut();
+        return null;
+      }
+
+      return token;
+    },
+  },
+  events: {
+    async signOut({ token }) {
+      if (token.provider === "keycloak") {
+        await signOutKeycloak(token);
+      }
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
