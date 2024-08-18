20d32ad99c97fb1f1cbf1008765be3475106e272
Dorokhovich Michael <mikhail@dorokhovich.ru>
2024-08-19 01:06:20 +0200

    FEAT: updated kafka usage - Mikhail Dorokhovich

--- data-compositor-service/adapters/test_adapter/app.py
+++ data-compositor-service/adapters/test_adapter/app.py
@@ -6,14 +6,28 @@ from src.adapter import Adapter
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
+@app.get("/{id}")
+async def root(id):
     neo4j_uri = os.environ['NEO4J_URI']
     neo4j_user = os.environ['NEO4J_USER']
     neo4j_password = os.environ['NEO4J_PASSWORD']
+
+    kafka_servers = os.environ['KAFKA_SERVERS']
+    kafka_user = os.environ['KAFKA_USER']
+    kafka_password = os.environ['KAFKA_PASSWORD']
+
     url = "https://webscraper.io/test-sites/e-commerce/allinone"
 
-    adapter = Adapter(neo4j_uri, neo4j_user, neo4j_password)
-    await adapter.scrape(url)
+    adapter = Adapter({
+            "uri": neo4j_uri,
+            "user": neo4j_user,
+            "password": neo4j_password
+        },
+        {
+            "servers": kafka_servers,
+            "user": kafka_user,
+            "password": kafka_password
+        })
+    await adapter.scrape(url, id)
     adapter.close()
     return JSONResponse(status_code=200, content={"message": "Scraping completed successfully"})
--- data-compositor-service/adapters/test_adapter/src/adapter.py
+++ data-compositor-service/adapters/test_adapter/src/adapter.py
@@ -1,13 +1,37 @@
+import json
+import socket
+
+from confluent_kafka import Producer
 from playwright.async_api import async_playwright
 from neo4j import GraphDatabase
 
 
 class Adapter:
 
-    def __init__(self, neo4j_uri, neo4j_user, neo4j_password):
-        self.neo4j_driver = GraphDatabase.driver(neo4j_uri, auth=(neo4j_user, neo4j_password))
+    def __init__(self, neo4j_config, kafka_config):
+        self.neo4j_driver = GraphDatabase.driver(
+            neo4j_config["uri"],
+            auth=(neo4j_config["user"], neo4j_config["password"])
+        )
+        self.kafka_config = kafka_config
+        if kafka_config.get('user'):
+            self.kafka_producer = Producer({
+                'bootstrap.servers': kafka_config.get("servers"),
+                'sasl.username': kafka_config.get("user"),
+                'sasl.password': kafka_config.get("password"),
 
-    async def scrape(self, url):
+                'security.protocol': 'PLAINTEXT',
+            })
+
+        else:
+            self.kafka_producer = Producer({
+                'bootstrap.servers': kafka_config.get("servers"),
+                'client.id': socket.gethostname(),
+
+                'security.protocol': 'PLAINTEXT',
+            })
+
+    async def scrape(self, url, task_id):
         async with async_playwright() as p:
             browser = await p.chromium.launch(headless=True)
             page = await browser.new_page()
@@ -27,9 +51,19 @@ class Adapter:
                             parent_url: $parent_url,
                             source: $source
                         })
-                        """, title=title_text, url=title_url, parent_url=url, source='viator')
+                    """, title=title_text, url=title_url, parent_url=url, source='test')
+
+            self.publish_to_kafka("data-compositor", "test_adapter", {"task_id": task_id, "url": url})
 
             await browser.close()
+
+    def publish_to_kafka(self, topic, key, value):
+        try:
+            self.kafka_producer.produce(topic, key=key, value=json.dumps(value).encode('utf-8'))
+            # self.kafka_producer.flush()
+            print(f"Message published to topic {topic}: {key} -> {value}")
+        except Exception as e:
+            print(f"Failed to publish message: {e}")
 
     def close(self):
         self.neo4j_driver.close()
